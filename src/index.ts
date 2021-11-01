10d542e0583a8de9007ac21995db375fde74ddea
Antony Merle <antony.merle@gmail.com>
2021-11-01 21:58:21 +0100

    login ok

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-import { Puppeteer } from "puppeteer";
+import puppeteer from "puppeteer";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/.env" });
 
 const login = async () => {
+  const login = process.env.LOGIN;
+  const mdp = process.env.PASSWORD;
+
   try {
     const URL = "https://www-cairn-info.rproxy.univ-pau.fr/";
-  } catch (error) {}
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(URL);
+
+    await page.type("#username", login as string);
+    await page.type("#password", mdp as string);
+
+    await Promise.all([
+      page.click("#fm1 > input.btn.btn-block.btn-submit"),
+      page.waitForNavigation(),
+    ]);
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+login();
