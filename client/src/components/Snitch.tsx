dab0bc2f9aefae1e8e474260909885d84923782a
Antony Merle <antony.merle@gmail.com>
2021-12-04 15:05:02 +0100

    cleanup

--- client/src/components/Snitch.tsx
+++ client/src/components/Snitch.tsx
@@ -1,13 +1,3 @@
-import { useState, useEffect } from "react";
-
-// interface SnitchLog {
-//   generalInfo: string[];
-//   test: string[];
-//   success: string[];
-//   failure: string[];
-//   report: string[];
-// }
-
 interface URLTableSchema {
   id: number;
   url: string;
@@ -29,13 +19,6 @@ interface Props {
 }
 
 const Snitch: React.FC<Props> = ({ report }) => {
-  // useEffect(() => {
-  //   // fetch("/report/34")
-  //   // fetch("/report/dernier")
-  //   fetch("/snitch")
-  //     .then((res) => res.json())
-  //     .then((data) => setLog(data));
-  // }, []);
   return (
     <div>
       <h3>Informations générales</h3>
@@ -62,11 +45,6 @@ const Snitch: React.FC<Props> = ({ report }) => {
       {report.success.map((line) => (
         <p key={line}>{line}</p>
       ))}
-
-      {/* <h3>Rapport</h3>
-      {report.report.map((line) => (
-        <p key={line.id}>{line.url}</p>
-      ))} */}
 
       <h3>Ressources testées : </h3>
       {report.report.map((line) => (
