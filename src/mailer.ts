12387d20bafd0e85334cbfc399604a1600ec8ce1
Antony Merle <antony.merle@univ-pau.fr>
2022-03-14 16:50:52 +0100

    yahoo mail boite générique

--- src/mailer.ts
+++ src/mailer.ts
@@ -19,25 +19,15 @@ interface URLTableSchema {
   report_id: number;
 }
 
-// https://www.hostinger.fr/tutoriels/utiliser-serveur-smtp-gmail
-// https://nodemailer.com/usage/using-gmail/  : accès moins sécurisé + captcha
-
-// async..await is not allowed in global scope, must use a wrapper
 export async function mailSender(rapport: SnitchLog) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-
   try {
     let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "smtp.mail.yahoo.com",
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
-        user: process.env.GMAILUSER, // generated ethereal user
-        pass: process.env.GMAILPASSWORD, // generated ethereal password
+        user: process.env.SMTPUSER,
+        pass: process.env.APPPASSWORD,
       },
     });
 
@@ -50,22 +40,15 @@ export async function mailSender(rapport: SnitchLog) {
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: '"Snitch 👻" <foo@snitch.com>', // sender address
-      to: "antony.merle@gmail.com", // list of receivers
-      subject: "Rapport d'analyse docelec ✔", // Subject line
-      // text: "Hello world?", // plain text body
-      html: mailHTML, // html body
+      from: "scd.uppa@yahoo.com",
+      to: "antony.merle@gmail.com",
+      subject: "👻 Rapport d'analyse docelec ✔",
+      html: mailHTML,
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (error) {
     console.log("Echec de l'envoi du mail de rapport:\n" + error);
   }
 }
-
-// mailSender().catch(console.error);
