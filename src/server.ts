33987993dd6242b0552fcc0abe20b6ff59231088
Antony Merle <antony.merle@univ-pau.fr>
2022-03-23 11:52:41 +0100

    maj dependencies

--- src/server.ts
+++ src/server.ts
@@ -558,7 +558,7 @@ app.get("/snitch", async (req, res) => {
   res.send(logs);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
